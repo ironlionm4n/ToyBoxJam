ff9a279239abf0402e94389fe4d8a316ec6355f9
rautry6 <94204738+rautry6@users.noreply.github.com>
2022-12-17 21:38:39 -0500

    Initial Jumping and Animation Hookups

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -2,49 +2,94 @@ using System;
 using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Required Components")]
     [SerializeField] private Rigidbody2D playerRigidbody;
-    [SerializeField] private float moveSpeed = 5f;
-    [SerializeField] private float jumpForce = 2;
-    [SerializeField] private float maxXVelocity = 2f;
     [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private Animator playerAnimator;
+
+    [Header("Running")]
+    [SerializeField] private float moveSpeed = 2f;
+    [SerializeField] private float maxXVelocity = 12f;
+    [SerializeField] private float moveHorizontal;
+
+    [Header("Jumping")]
+    [SerializeField] private float jumpForce = 20;
+    [SerializeField] private bool isJumping = false;
+    [SerializeField] private float fallForce = 5f;
+    [SerializeField] private float moveVertical;
 
     private Vector2 _moveDirection = Vector2.zero;
     private bool _shouldJump;
 
+    private void Awake()
+    {
+        playerRigidbody.gravityScale = fallForce;
+    }
+
     private void Update()
     {
-        var horizontal = Input.GetAxis("Horizontal");
-        
-        if (horizontal < 0)
+        moveHorizontal = Input.GetAxisRaw("Horizontal");
+        moveVertical = Input.GetAxisRaw("Vertical");
+
+        //Sets animator values
+        playerAnimator.SetFloat("HorizontalMovement", moveHorizontal);
+        playerAnimator.SetFloat("VerticalMovement", playerRigidbody.velocity.y);
+
+        if (moveHorizontal < 0)
             spriteRenderer.flipX = true;
-        if (horizontal > 0)
+        if (moveHorizontal > 0)
             spriteRenderer.flipX = false;
         
-        _moveDirection = new Vector2(horizontal, 0f);
+
         if (Input.GetKeyDown(KeyCode.W))
         {
+            playerAnimator.SetBool("Jumping", true);
             _shouldJump = true;
         }
     }
 
     private void FixedUpdate()
     {
-        if (_shouldJump) Jump();
-        playerRigidbody.AddForce(_moveDirection * moveSpeed);
+        if (moveHorizontal > 0.1f)
+        {
+            if (playerRigidbody.velocity.x < maxXVelocity)
+            {
+                playerRigidbody.AddForce(new Vector2(moveHorizontal * moveSpeed, 0f), ForceMode2D.Impulse);
+            }
+        }
 
-        if (playerRigidbody.velocity.y < 0)
+        if (moveHorizontal < -0.1f)
         {
-            playerRigidbody.gravityScale = 3f;
+            if (playerRigidbody.velocity.x > -maxXVelocity)
+            {
+                playerRigidbody.AddForce(new Vector2(moveHorizontal * moveSpeed, 0f), ForceMode2D.Impulse);
+            }
         }
-        else
-        {
-            playerRigidbody.gravityScale = 1f;
-        }
+
+        if (_shouldJump && !isJumping) Jump();
+        
     }
 
     private void Jump()
     {
-        playerRigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        playerRigidbody.AddForce(new Vector2(0f, moveVertical * jumpForce), ForceMode2D.Impulse);
         _shouldJump = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isJumping = false;
+            playerAnimator.SetBool("Jumping", false);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isJumping = true;
+        }
     }
 }
