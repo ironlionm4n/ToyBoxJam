0dd9ea475acabc6dd7fdea54b40623bd774bfb7a
ironlionm4n <ironlionm4n@gmail.com>
2022-12-28 22:44:07 -0500

    Main menu play button SFX

--- Assets/Scripts/Gameplay/SceneManagerController.cs
+++ Assets/Scripts/Gameplay/SceneManagerController.cs
@@ -8,9 +8,18 @@ public class SceneManagerController : MonoBehaviour
 {
     public void LoadMainGame()
     {
+        var playButtonAudioSource = GetComponent<AudioSource>();
+        var playButtonAudioClipLength = playButtonAudioSource.clip.length;
+        StartCoroutine(LoadPlatformSection(playButtonAudioClipLength));
+        
+    }
+
+    private IEnumerator LoadPlatformSection(float playButtonAudioClipLength)
+    {
+        yield return new WaitForSeconds(playButtonAudioClipLength / 3f);
         SceneManager.LoadScene("PlatformSection");
     }
-    
+
     public void LoadOptions()
     {
         SceneManager.LoadScene("Options", LoadSceneMode.Additive);
